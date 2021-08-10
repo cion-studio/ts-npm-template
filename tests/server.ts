df881e6e7efc1cda0ba54062ab580da14d5d7948
Youssof Fahmy <youssoffahmy@gmail.com>
2021-08-09 22:01:47 -0400

    Updated

--- tests/server.ts
+++
@@ -1,8 +0,0 @@
-import beamCreator from '../src/beamCreator'
-import fetchHandler from 'node-fetch'
-
-const Beam = beamCreator(fetchHandler)
-
-const beam = new Beam()
-
-beam.get('https://api.kanye.rest/').then(console.log).catch(console.error)
